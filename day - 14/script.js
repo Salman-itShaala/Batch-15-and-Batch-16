0ca0c88f7e6c6be2fb20c092cd99eb5badcccf5d
salman-itShaala <salman.shaikh@itshaala.com>
2025-05-19 17:47:30 +0530

    added operators code

---
+++ day - 14/script.js
@@ -0,0 +1,68 @@
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
+console.log(false && false);
+
+// check canVote
+
+let age = 9;
+
+// ternary operator --> syntax --> condition ? code if true : code if false;
+age >= 18 ? console.log("you can vote") : console.log("sorry, You can not vote")
+
+
+
+// Compound assignment operators
+
+age /= 5; // age = age / 5;
+
+// +=
+// -=
+// /=
+// *=
+// %=
+// **=
+
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+// console.log(age >= 21 && gender === "male");
+
+
+// console.log(age >= 21);
+// console.log(gender == "male");
+
+// OR operator ( || )
+
+// console.log(false || false);
+// console.log(false || true);
+// console.log(true || false)
+// console.log(true || true)
+
+
+// // NOT operator (!)
+// console.log(!true);// false
+// console.log(!false); // true
+
+
+
+// let num = 1;
+
+// console.log(false || num++);
+
+// console.log(num); // 2
+
+// // 1  - 4
+// // 2  - 3
+
+
+
