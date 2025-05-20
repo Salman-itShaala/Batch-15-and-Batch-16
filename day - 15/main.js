ad4a4ee6df95f681c178526c73457edb3526c78f
salman-itShaala <salman.shaikh@itshaala.com>
2025-05-20 17:45:14 +0530

    added descision making code

---
+++ day - 15/main.js
@@ -0,0 +1,53 @@
+let age = 55;
+
+// Descision making statements
+
+// 1. If 
+
+if (age >= 18) {
+    console.log("You can vote");
+}
+
+
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("you can not vote");
+}
+
+
+// given number is positive
+
+let num = 10;
+
+if (num > 0) {
+    console.log("Number is positive");
+} else if (num == 0) {
+    console.log("Given number is zero")
+} else {
+    console.log("Number is negative");
+}
+
+
+
+let language = "french"; // english, hindi, 
+
+if (language == "marathi") {
+    console.log("Jai maharashtra");
+} else if (language == "english") {
+    console.log("Hello");
+} else if (language == "hindi") {
+    console.log("Namaste");
+} else {
+    console.log("Invalid language");
+}
+
+
+let number = 21;
+
+if (number % 2 == 0) {
+    console.log("Given number is Even");
+} else {
+    console.log("Given number is Odd");
+}
---
+++ day - 15/script.js
@@ -0,0 +1,13 @@
+let firstName = "Salman";
+let city = "Pune"
+
+
+let sentence = "My name is " + firstName + " I live in " + city;
+
+// string template
+let sentence2 = `My name is ${firstName} I live in ${city}`;
+
+// My name is fistName I live in city
+
+console.log(sentence);
+console.log(sentence2);
