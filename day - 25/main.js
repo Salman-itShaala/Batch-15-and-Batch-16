e2bae8838bf9835c240eddd64d88365fe71a6e4c
salman-itShaala <salman.shaikh@itshaala.com>
2025-06-05 15:56:46 +0530

    added code

---
+++ day - 25/main.js
@@ -0,0 +1,13 @@
+
+const para = document.getElementById("para")
+
+// para.classList.add()
+// .remove()
+// .contains()
+// .toggle()
+
+
+function changeStyles() {
+    para.classList.toggle("my-para")
+
+}
---
+++ day - 25/script.js
@@ -0,0 +1,14 @@
+const img = document.getElementById("img");
+
+// attribute --> add, remove
+
+// img.setAttribute("src", "value")
+// img.removeAttribute("src");
+// img.getAttribute("src") --> it will return you value of attribute
+
+const newPara = document.createElement("p");
+newPara.innerText = "This is a new paragraph.";
+
+function changeAtt() {
+
+}
