0d54d8b6fa1cd1a61b4930c4b89468639d0f4826
salman-itShaala <salman.shaikh@itshaala.com>
2025-07-02 11:54:37 +0530

    added portfolio code

--- day - 37/portfolio/main.js
+++ day - 37/portfolio/main.js
@@ -1,3 +1,11 @@
+const skillsDiv = document.getElementById("skills-div");
+const projectDiv = document.getElementById("projects-div");
+
+const skillsBtn = document.getElementById("skills-btn");
+const projectsBtn = document.getElementById("projects-btn")
+
+// function to toggle theme
+
 function toggleTheme() {
     const htmlEl = document.documentElement;
     const sun = document.getElementById("sun");
@@ -6,4 +14,26 @@ function toggleTheme() {
     htmlEl.classList.toggle("dark");
     sun.classList.toggle("hide");
     moon.classList.toggle("hide");
+}
+
+
+function displayProjects() {
+    //
+    skillsDiv.classList.add("hidden-content");
+    projectDiv.classList.remove("hidden-content");
+
+
+    skillsBtn.style.backgroundColor = "var(--theme-btn-bg)";
+    projectsBtn.style.backgroundColor = "var(--bg-color)";
+
+}
+
+function displaySkills() {
+    // 
+    skillsDiv.classList.remove("hidden-content");
+    projectDiv.classList.add("hidden-content");
+
+
+    skillsBtn.style.backgroundColor = "var(--bg-color)";
+    projectsBtn.style.backgroundColor = "var(--theme-btn-bg)";
 }
