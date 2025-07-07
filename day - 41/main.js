ffa49baa29ebc0dfa5139fc9c7348cef6fa9b9da
salman-itShaala <salman.shaikh@itshaala.com>
2025-07-07 12:53:04 +0530

    added arrays code

---
+++ day - 41/main.js
@@ -0,0 +1,23 @@
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+
+// for (let name of names) {
+//     console.log(name);
+// }
+
+
+
+const names = ["a", "z", "s", "c", "f", "b"];
+
+// arrow function
+
+names.forEach((name) => {
+    console.log(name);
+})
+
+
+
+
---
+++ day - 41/script.js
@@ -0,0 +1,30 @@
+const marks = [67, 87, 44, 59, 98, 71, 100, 88];
+
+// functions
+
+// pop --> will delete last element and returns it
+// push --> we can add element at the end and it will return new length
+// shift --> we can delete first element
+// unshift --> we can add element at first position
+// splice --> we can add / delete elements wherever we want
+
+// marks[0] --> marks.at(0)
+// marks[1] --> marks.at(1)
+// marks[2] --> marks.at(2)
+
+// at --> used to retrive elements
+// includes --> to search elements will return Boolean
+// join --> Adds all the elements of an array into a string, separated by the specified separator string.
+// slice --> Returns a copy of a section of an array.
+
+// 
+
+console.log(marks)
+
+console.log(marks.reverse());
+
+const names = ["a", "z", "s", "c", "f", "b"];
+
+console.log(names.sort());
+
+console.log(marks.sort());
